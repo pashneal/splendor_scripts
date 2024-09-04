409519d3080d89444fa7addc48d8b19ef7fb1521
Neal Powell <pashneal@gmail.com>
2024-09-03 20:28:45 -0400

    chore: bump version

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const VERSION: &str = "0.1.4";
+pub const VERSION: &str = "0.1.5";
 pub const STOURNEY_ARENA_REPO_URL : &str = "https://github.com/pashneal/stourney_tournament";
 
 pub const CONF_FILE_NAME : &str = "stourney.conf";
