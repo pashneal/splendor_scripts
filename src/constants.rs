659220ddd4c3a36a2e04dc8c13865529a3c0758b
Neal Powell <pashneal@gmail.com>
2024-09-21 21:46:16 -0400

    chore: delete unused reference

--- src/constants.rs
+++ src/constants.rs
@@ -2,4 +2,3 @@ pub const VERSION: &str = "0.1.10";
 pub const STOURNEY_ARENA_REPO_URL: &str = "https://github.com/pashneal/stourney_tournament";
 
 pub const CONF_FILE_NAME: &str = "stourney.conf";
-pub const STOURNEY_WATCH_WEBSOCKET_URL : &str = "ws://127.0.0.1:3031/ws";
