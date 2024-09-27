f6c11fdc46443aede67b8a45ce4f87f4e9659a6d
Neal Powell <pashneal@gmail.com>
2024-09-27 17:20:37 -0400

    chore: update from upstream deps

--- src/constants.rs
+++ src/constants.rs
@@ -3,7 +3,7 @@ pub const CONF_FILE_NAME: &str = "stourney.conf";
 /// The version of the config file format
 /// be sure to match this with the Cargo.toml version if the
 /// config file format changes
-pub const VERSION: &str = "0.1.14";
+pub const VERSION: &str = "0.1.15";
 
 pub const STOURNEY_ARENA_REPO_URL: &str = "https://github.com/pashneal/stourney_tournament";
 
